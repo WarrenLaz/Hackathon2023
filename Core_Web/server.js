c5a510dd9f8287cd4276b67de6d2187826a0986b
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2023-10-21 17:48:53 -0400

    changed
    here

---
+++ Core_Web/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer(function(req,res){
+   res.writeHead(200, { 'Content-Type': 'text/html'})
+   fs.readFile('/Users/warren_lazarraga/Programming_projects/Hackathon2023/Core_Web/core.html', function(error,data){
+    if(error){
+        res.writeHead(404)
+        res.write('Error: File Not Found')
+    } else {
+        res.write(data)
+    }
+    res.end()
+   })
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('wrong')
+    } else{
+        console.log('server is listening on port ' + port)
+    }
+})
